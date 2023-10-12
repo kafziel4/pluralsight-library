859ff4e57ecea1518909c3c476609708aa30c12f
Keoma Kevin Baudin <keoma4@gmail.com>
2023-10-12 00:38:53 -0300

    Add project files.

---
+++ Library.API/Controllers/AuthorsControllerV2.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    public class AuthorsControllerV2 : ControllerBase
+    {
+        private readonly IAuthorRepository _authorsRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsControllerV2(
+            IAuthorRepository authorsRepository,
+            IMapper mapper)
+        {
+            _authorsRepository = authorsRepository;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get a list of authors, v2
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            var authorsFromRepo = await _authorsRepository.GetAuthorsAsync();
+            return Ok(_mapper.Map<IEnumerable<Author>>(authorsFromRepo));
+        }
+    }
+}
